d774ce7b3542bdcf7bd0867d68467afe2ca2e79b
Phil Kedy <phil.kedy@gmail.com>
2022-09-19 09:21:52 -0400

    Watch config files and load relative to CWD (#7)

--- watch.go
+++ watch.go
@@ -18,7 +18,6 @@ package cli
 
 import (
 	"log"
-	"os"
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
@@ -28,73 +27,149 @@ type WatchCmd struct {
 	Configs []string `arg:"" help:"The code generation configuration files" type:"existingfile" optional:""`
 }
 
-type ConfigAndPath struct {
-	Config
-	Path string
-}
-
 func (c *WatchCmd) Run(ctx *Context) error {
-	path, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
 	if len(c.Configs) == 0 {
 		c.Configs = append(c.Configs, "apex.yaml")
 	}
-
-	specs := make(map[string][]ConfigAndPath)
-	for _, config := range c.Configs {
-		configPath := filepath.Dir(config)
-		fileConfigs, err := readConfigs(config)
+	for i, config := range c.Configs {
+		config, err := filepath.Abs(config)
 		if err != nil {
 			return err
 		}
-
-		for _, config := range fileConfigs {
-			specFile := filepath.Join(configPath, config.Spec)
-			configs := specs[specFile]
-			configs = append(configs, ConfigAndPath{config, configPath})
-			specs[specFile] = configs
-		}
+		c.Configs[i] = config
 	}
 
-	watcher, err := fsnotify.NewWatcher()
+	configs := make(map[string][]string)
+	specs := make(map[string][]Config)
+
+	reloadConfigs := func() error {
+		configs = make(map[string][]string)
+		specs = make(map[string][]Config)
+
+		for _, config := range c.Configs {
+			fileConfigs, err := readConfigs(config)
+			if err != nil {
+				return err
+			}
+
+			configSpecs := []string{}
+			for _, config := range fileConfigs {
+				specFile, err := filepath.Abs(config.Spec)
+				if err != nil {
+					return err
+				}
+				configSpecs = append(configSpecs, specFile)
+				configs := specs[specFile]
+				configs = append(configs, config)
+				specs[specFile] = configs
+			}
+			configs[config] = configSpecs
+		}
+
+		return nil
+	}
+
+	configWatcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
 	}
-	defer watcher.Close()
+	defer configWatcher.Close()
+
+	specWatcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer specWatcher.Close()
+
+	syncWatchers := func() error {
+		currentSpecs := make(map[string]struct{})
+		removeSpecs := make(map[string]struct{})
+		for _, name := range specWatcher.WatchList() {
+			currentSpecs[name] = struct{}{}
+			removeSpecs[name] = struct{}{}
+		}
+		for _, specs := range configs {
+			for _, spec := range specs {
+				if _, exists := currentSpecs[spec]; exists {
+					delete(removeSpecs, spec)
+					continue
+				}
+				log.Printf("Watching %s...", spec)
+				if err = specWatcher.Add(spec); err != nil {
+					return err
+				}
+				currentSpecs[spec] = struct{}{}
+			}
+		}
+		for name := range removeSpecs {
+			log.Printf("Unwatching %s...", name)
+			specWatcher.Remove(name)
+		}
+
+		return nil
+	}
 
 	done := make(chan bool)
+
 	go func() {
 		for {
 			select {
-			case event, ok := <-watcher.Events:
+			case event, ok := <-configWatcher.Events:
 				if !ok {
 					return
 				}
-
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("Modified spec:", event.Name)
-					g := GenerateCmd{}
-					configs := specs[event.Name]
-					for _, config := range configs {
-						if err := os.Chdir(config.Path); err != nil {
-							log.Printf("Error running generate: %v", err)
-							continue
-						}
-
-						if g.generateConfig(config.Config); err != nil {
-							log.Printf("Error running generate: %v", err)
-						}
-					}
-
-					os.Chdir(path)
-
-					log.Println("Watching for file changes.")
+				if event.Op&fsnotify.Write != fsnotify.Write {
+					continue
 				}
 
-			case err, ok := <-watcher.Errors:
+				log.Println("Modified config:", event.Name)
+				if err := reloadConfigs(); err != nil {
+					log.Println("error:", err)
+					return
+				}
+				if err := syncWatchers(); err != nil {
+					log.Println("error:", err)
+					return
+				}
+
+				g := GenerateCmd{}
+				if eventSpecs, ok := configs[event.Name]; ok {
+					for _, eventSpec := range eventSpecs {
+						configs := specs[eventSpec]
+						for _, config := range configs {
+							if g.generateConfig(config); err != nil {
+								log.Printf("Error running generate: %v", err)
+							}
+						}
+					}
+				}
+
+			case event, ok := <-specWatcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write != fsnotify.Write {
+					continue
+				}
+
+				log.Println("Modified spec:", event.Name)
+				g := GenerateCmd{}
+				configs := specs[event.Name]
+				for _, config := range configs {
+					if g.generateConfig(config); err != nil {
+						log.Printf("Error running generate: %v", err)
+					}
+				}
+
+				log.Println("Watching for file changes.")
+
+			case err, ok := <-configWatcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+
+			case err, ok := <-specWatcher.Errors:
 				if !ok {
 					return
 				}
@@ -103,12 +178,14 @@ func (c *WatchCmd) Run(ctx *Context) error {
 		}
 	}()
 
-	for spec := range specs {
-		log.Printf("Watching %s...", spec)
-		if err = watcher.Add(spec); err != nil {
+	reloadConfigs()
+	for config := range configs {
+		log.Printf("Watching %s...", config)
+		if err = configWatcher.Add(config); err != nil {
 			log.Fatal(err)
 		}
 	}
+	syncWatchers()
 
 	log.Println("Watching for file changes.")
 	<-done
